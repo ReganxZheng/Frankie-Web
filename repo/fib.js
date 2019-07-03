9533d45187e30186af3ab1e5fedffe6d2bef285a
Zheng JinWenHao <frankie@Zhengs-MacBook-Pro.local>
2019-07-04 00:08:54 +1200

    ES6 fixed

--- repo/fib.js
+++ repo/fib.js
@@ -1,13 +1,17 @@
-function fib(n) {
-  if (n<0){
-    document.getElementById("result").innerHTML = "Invalid number!";
+var fib = (n) => {
+  const obj = {
+    msg: "Invalid number",
+    arr: [0, 1],
   }
-  else {
-    let arr = [0, 1];
-    for (var i = 2; i < n + 1; i++) {
+  if (n < 0) {
+    let {msg} = obj;
+    document.getElementById("result").innerHTML = msg;
+  } else {
+    let {arr} = obj;
+    for (let i = 2; i < n + 1; i++) {
       arr.push(arr[i - 2] + arr[i - 1]);
     }
-    document.getElementById("result").innerHTML = "Result: " + arr;
+    document.getElementById("result").innerHTML = arr;
     document.getElementsByName("fibnum")[0].value = "";
   }
 }
