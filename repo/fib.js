6a7af8f18d7e50f7f800c0197283b266d5b36620
Zheng JinWenHao <frankie@Zhengs-MacBook-Pro.local>
2019-07-03 20:18:56 +1200

    minor fix

--- repo/fib.js
+++ repo/fib.js
@@ -1,9 +1,13 @@
 function fib(n) {
-  let arr = [0, 1];
-
-  for(var i=2;i<n+1;i++) {
-    arr.push(arr[i-2] + arr[i-1]);
+  if (n<0){
+    document.getElementById("result").innerHTML = "Invalid number!";
   }
-  document.getElementById("result").innerHTML = "Result: " + arr;
-  document.getElementsByName("fibnum")[0].value="";
+  else {
+    let arr = [0, 1];
+    for (var i = 2; i < n + 1; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    document.getElementById("result").innerHTML = "Result: " + arr;
+    document.getElementsByName("fibnum")[0].value = "";
+  }
 }
